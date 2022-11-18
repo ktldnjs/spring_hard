c69d56c5e9d7a5c678814b77a2fabd58275c4ec6
siwonkim <ktldnjs0811@naver.com>
2022-11-18 16:30:30 +0900

    스프링 심화반 프로젝트 생성

---
+++ spring_hard/src/main/java/com/study/spring_hard/SpringHardApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring_hard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHardApplication.class, args);
+    }
+
+}
---
+++ spring_hard/src/test/java/com/study/spring_hard/SpringHardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring_hard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
