041fd27a1e247190eb513b26a3f988876b7e65fa
siwonkim <ktldnjs0811@naver.com>
2022-11-21 21:16:21 +0900

    servlet으로 상품 검색 API 구현

--- spring_hard/src/main/java/com/study/spring_hard/SpringHardApplication.java
+++ spring_hard/src/main/java/com/study/spring_hard/SpringHardApplication.java
@@ -2,7 +2,9 @@ package com.study.spring_hard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // @WebServlet 어노테이션이 동작하게 함.
 @SpringBootApplication
 public class SpringHardApplication {
 
